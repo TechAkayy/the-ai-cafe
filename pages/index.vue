e49fde26eafa914ca02a520ff8e32f7bdcf92b3d
Akayy <techakayy@gmail.com>
2023-11-11 15:54:44 +1100

    06 NavBar

--- pages/index.vue
+++ pages/index.vue
@@ -36,20 +36,7 @@
 </script>
 <template>
   <div>
-    <div
-      style="
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        justify-content: center;
-        padding-top: 1.5rem /* 24px */;
-        padding-bottom: 1.5rem /* 24px */;
-      "
-    >
-      <DarkModeSwitch /><span style="margin-left: 0.5rem; /* 8px */"
-        >A Fresh Start!</span
-      >
-    </div>
+    <NavBar></NavBar>
   </div>
 </template>
 <style scoped></style>
